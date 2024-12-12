9ac8c78f6709335cdf36470c179a8c5cf77278fd
SiXMaRs <wuttinan.lu.65@ubu.ac.th>
2024-12-12 13:01:48 +0700

    up

--- te/cou/urls.py
+++ te/cou/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import *
+
 urlpatterns = [
-    path('', courselist, name='list')
+    path('list/', courselist, name='list'),
+    path('se/', ser_co, name='ser')
 ]
--- te/cou/views.py
+++ te/cou/views.py
@@ -4,4 +4,9 @@ from .models import *
 # Create your views here.
 def courselist(request):
     co = course.objects.all()
-    return render(request, 'listco', {'corse': co})
+    return render(request, 'listco.html', {'corse': co})
+
+def ser_co(request):
+    se = request.GET.get('id', '')
+    cou = course.objects.filter(id=se)
+    return render(request, 'ser.html',{'course': cou})
